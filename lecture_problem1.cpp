4c9c3c844454dcf2c01ec5a0a307eeb842e1d6a0
owencurt <owenpcurtis@outlook.com>
2025-09-12 13:33:03 -0400

    Pick Random Num

---
+++ lecture_problem1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <random>
+#include <ctime>
+
+int main() {
+    int target_number{};
+    target_number = rand();
+
+    std::cout << target_number << "\n";
+    
+
+    return 0;
+}
