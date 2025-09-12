4d4ec715b6c96b3a8c0976b60258d23653c38e90
owencurt <owenpcurtis@outlook.com>
2025-09-12 13:43:45 -0400

    Finish logic and implement more of a 'real' random number using srand(time)

--- lecture_problem1.cpp
+++ lecture_problem1.cpp
@@ -4,11 +4,31 @@
 #include <ctime>
 
 int main() {
+    srand(time(0));
     int target_number{};
-    target_number = rand();
+    int guess{};
+    target_number = rand() % 101;
 
     std::cout << target_number << "\n";
     
+    
+    while (true) {
+        std::cout << "Enter Your Guess: ";
+        std::cin >> guess;
 
+        if (guess > target_number) {
+            std::cout << "The number is lower than your guess" << "\n ";
+            continue;
+        }
+        else if (guess < target_number) {
+            std::cout << "The number is higher than your guess" << "\n ";
+            continue;
+        }
+        else {
+            std::cout << "You guessed it correct! " << "\n ";
+            break;
+        }
+    }
+    
     return 0;
 }
