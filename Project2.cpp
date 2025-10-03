eac5148175ead35a26e0614a88f77f1472effa4a
owencurt <owenpcurtis@outlook.com>
2025-10-03 12:33:00 -0400

    Project 2

--- Project2.cpp
+++ Project2.cpp
@@ -1,15 +1,25 @@
 // WRITE YOUR CODE HERE
 #include <iostream>
 #include <string>
+#include <vector>
 
 int NumberOfCharactersOverlap(std::string str1, std::string str2) {
-  int count{};
-  for (int i = 0; i < str1.size(); ++i) {
-    if (str1.at(i) == str2.at(count)) {
-      count += 1;
+    int maxLen = std::min(str1.size(), str2.size());
+
+    for (int len = maxLen; len > 0; --len) {
+        bool match = true;
+        for (int j = 0; j < len; ++j) {
+            if (str1.at(str1.size() - len + j) != str2.at(j)) {
+                match = false;
+                break;
+            }
+        }
+        if (match) {
+            return len;
+        }
     }
-  }
-  return count;
+
+    return 0;
 }
 
 std::string CombineWords(std::string str1, std::string str2, int count) {
@@ -20,15 +30,41 @@ std::string CombineWords(std::string str1, std::string str2, int count) {
     return str1 +  " " + str2;
 }
 
-int main(){
-  std::string str1 = "dog";
-  std::string str2 = "cheese";
-  int count{};
-  count = NumberOfCharactersOverlap(str1, str2);
+std::vector<std::string> SplitWords(const std::string& input) {
+  std::vector<std::string> words;
+  std::string word;
 
-  std::string result{};
-  result = CombineWords(str1, str2, count);
+  for (char c : input) {
+    if (c == ' ') {
+        if (!word.empty()) {
+            words.push_back(word);
+            word.clear();
+        }
+    } else {
+        word += c;
+    }
+  }
+  if (!word.empty()) {
+      words.push_back(word);
+  }
+
+  return words;
+}
+
+int main(){
+  std::string line{};
+  while (std::getline(std::cin, line)) {
+    std::vector<std::string> words = SplitWords(line);
+
+    std::string result = words[0];
+    for (size_t i = 1; i < words.size(); ++i) {
+        int count = NumberOfCharactersOverlap(result, words[i]);
+        result = CombineWords(result, words[i], count);
+  }
 
   std::cout << result << "\n";
+  }
+
+  
   return 0;
 }
