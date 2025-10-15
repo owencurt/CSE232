3d7fecfd152ada2f116921c5851b599e9f573232
owencurt <owenpcurtis@outlook.com>
2025-10-15 10:09:09 -0400

    Finished Project 3

--- Projects/Project3.cpp
+++ Projects/Project3.cpp
@@ -290,9 +290,13 @@ std::string process_command(std::string line, std::string& previous_date, double
     if (!previous_date.empty() && previous_date != date) {
         int months = number_of_first_of_months(previous_date, date);
         if (months > 0) {
-            double interest = interest_earned(balance, apr, previous_date, date);
-            balance += interest;
-            balance = round_to_cent(balance);
+            double interest = 0.0;
+
+            if (balance > 0) {
+                interest = interest_earned(balance, apr, previous_date, date);
+                balance += interest;
+                balance = round_to_cent(balance);
+            }
 
             std::ostringstream interest_str;
             interest_str << std::fixed << std::setprecision(2) << interest;
@@ -302,7 +306,6 @@ std::string process_command(std::string line, std::string& previous_date, double
         }
     }
 
-    
 
     if (command == "Deposit") {
         deposit(balance, amount);
@@ -321,6 +324,43 @@ std::string process_command(std::string line, std::string& previous_date, double
     out << std::fixed << std::setprecision(2) << balance;
 
     result += "Balance: " + out.str() + "\n";
+
+    return result;
+}
+
+
+// // The "process_commands" function is the last function you need to write. 
+// // Parameters: 
+// // a string (representing a multiline to be processed),
+// // a double (representing the apr).
+// // The function should return a string (possibly with multiple lines) indicating the result of performing the operations.
+// std::string input = 
+// "2016-09-02 Deposit $500\n"
+// "2016-09-02 Withdraw $23.54\n"
+// "2016-09-04 Withdraw $58\n"
+// "2016-11-02 Deposit $200\n"
+// "2017-03-02 Withdraw $1000\n"
+// "2019-11-02 Deposit $5000\n"
+// "2020-11-02 Deposit $500\n";
+// std::string result = process_commands(input, 7);
+// std::string expected = "On 2016-09-02: Instructed to perform \"Deposit $500\"\nBalance: 500.00\nOn 2016-09-02: Instructed to perform \"Withdraw $23.54\"\nBalance: 476.46\nOn 2016-09-04: Instructed to perform \"Withdraw $58\"\nBalance: 418.46\nOn 2016-11-02: Instructed to perform \"Deposit $200\"\nSince 2016-09-04, interest has accrued 2 times.\n$4.89 interest has been earned.\nBalance: 623.35\nOn 2017-03-02: Instructed to perform \"Withdraw $1000\"\nSince 2016-11-02, interest has accrued 4 times.\n$14.65 interest has been earned.\nOverdraft!\nBalance: -397.00\nOn 2019-11-02: Instructed to perform \"Deposit $5000\"\nSince 2017-03-02, interest has accrued 32 times.\n$0.00 interest has been earned.\nBalance: 4603.00\nOn 2020-11-02: Instructed to perform \"Deposit $500\"\nSince 2019-11-02, interest has accrued 12 times.\n$332.69 interest has been earned.\nBalance: 5435.69\n";
+// std::cout << "Expected:" << std::endl << expected << std::endl;
+// std::cout << "Result:" << std::endl << result << std::endl;
+// CHECK(result == expected);
+std::string process_commands(std::string input, double apr) {
+    std::stringstream ss(input);
+    std::string line;
+    std::string previous_date = "";
+    double balance = 0.0;
+    std::string result;
+
+    while (std::getline(ss, line)) {
+        if (line.empty()) continue;
+
+        std::string line_result = process_command(line, previous_date, balance, apr);
+
+        result += line_result;
+    }
 
     return result;
 }
